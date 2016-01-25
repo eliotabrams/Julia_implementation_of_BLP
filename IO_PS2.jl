19f712305e8381f9d67a30f80363273fe90c8bd8
hyunminpark <parkh@uchicago.edu>
2016-01-24 21:36:30 -0600

    updated IO_PS2

--- IO_PS2.jl
+++ IO_PS2.jl
@@ -16,10 +16,11 @@ iv = hcat(x,z)
 
 # Setting up the model
 using JuMP
-N = size(z,2)+size(x,1)+1+size(x,2)
 L = size(x,2)+size(z,2)
 J = size(x,1)
+N = L + J + 1 + size(x,2)
 m = Model()
+
 
 # theta = (g xi alpha beta)
 @defVar(m,theta[1:N])
@@ -27,24 +28,27 @@ m = Model()
 
 # g = sum_j xi_j iv_j
 l = 1
-while l < = L
+while l <= L
 	@addConstraint(m,theta[l]==sum{theta[L+j]*iv[j,l],j=1:J}) 
+	l += 1
 end
 
 # market share equations (loop)
 j = 1
 while j <= J
-	@addNLConstraint(m,theta[L+j]==log(s[j])-log(s0[j])+theta[L+J+1]*p[j]-theta[L+J+2]*x[j,1]-theta[L+J+3]*x[j,2]-theta[L+J+4]*x[j,3])
+	@addNLConstraint(m,theta[L+j]==log(s[j])-log(s0[j])+theta[L+J+1]*p[j]-theta[L+J+2]*x[j,1]-theta[L+J+3]*x[j,2]-theta[L+J+4]*x[j,3]-theta[L+J+5]*x[j,4])
 	j += 1
 end
 
 using Ipopt
-setSolver(m,IpoptSolver(tol = 1e-8, max_iter = 200, output_file = "results.txt"))
+setSolver(m,IpoptSolver(tol = 1e-10, max_iter = 200, output_file = "results.txt"))
 
 for i=1:N
-  setValue(x[i],3)
+  setValue(theta[i],1)
 end
 
 status = solve(m)
 print(status)
 
+println("alpha = ", getValue(theta[539]))
+println("beta = ", getValue(theta[540:543]))
