4598c136d4ef0ff323d3dbf910f5a0b9781d8b04
hyunminpark <parkh@uchicago.edu>
2016-01-27 21:55:34 -0600

    blp added

--- IO_PS2_logit.jl
+++ IO_PS2_logit.jl
@@ -17,41 +17,44 @@ iv = hcat(x,z)
 
 # Setting up the model
 using JuMP
-L = size(x,2)+size(z,2)
+K = size(x,2)
+L = K+size(z,2)
 J = size(x,1)
-N = L + J + 1 + size(x,2)
 m = Model()
 
 
-# theta = (g xi alpha beta)
-@defVar(m,theta[1:N])
-@setObjective(m,Min,sum{theta[l]^2,l=1:L})
+# Defining variables
+@defVar(m, g[1:L])
+@defVar(m, xi[1:J])
+@defVar(m, alpha)
+@defVar(m, beta[1:K])
+@setObjective(m,Min,sum{g[l]^2,l=1:L})
 
 # g = sum_j xi_j iv_j
 l = 1
 while l <= L
-	@addConstraint(m,theta[l]==sum{theta[L+j]*iv[j,l],j=1:J}) 
+	@addConstraint(m,g[l]==sum{xi[j]*iv[j,l],j=1:J}) 
 	l += 1
 end
 
 # market share equations (loop)
 j = 1
 while j <= J
-	@addNLConstraint(m,theta[L+j]==log(s[j])-log(s0[j])+theta[L+J+1]*p[j]-theta[L+J+2]*x[j,1]-theta[L+J+3]*x[j,2]-theta[L+J+4]*x[j,3]-theta[L+J+5]*x[j,4])
+	@addNLConstraint(m,xi[j]==log(s[j])-log(s0[j])+alpha*p[j]-sum{beta[k]*x[j,k],k=1:K})
 	j += 1
 end
 
 using Ipopt
 setSolver(m,IpoptSolver(tol = 1e-10, max_iter = 200, output_file = "results.txt"))
 
-for i=1:N
-  setValue(theta[i],1)
-end
+#for l=1:L
+#  setValue(g[l],1)
+#end
 
 status = solve(m)
 print(status)
 
-println("alpha = ", getValue(theta[539]))
-println("beta = ", getValue(theta[540:543]))
+println("alpha = ", getValue(alpha))
+println("beta = ", getValue(beta[1:K]))
 
 toc()
